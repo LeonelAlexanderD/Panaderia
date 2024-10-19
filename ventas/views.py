4846457b8084fad9267624a710f61a8d3ae4785d
Alex <chaffs25@gmail.com>
2024-10-19 19:34:51 -0300

    Revert "Feature/ventas"

--- ventas/views.py
+++ ventas/views.py
@@ -1,14 +1,3 @@
 from django.shortcuts import render
 
-from ventas.models import Comprobante
-
 # Create your views here.
-def nueva_venta(request):
-    return render(request, 'ventas/venta.html')
-
-def pagar(request):
-    return render(request, 'ventas/genrerar_comprobante.html')
-
-def listar_ventas(request):
-    comprobantes = Comprobante.object.all()
-    return render(request, 'ventas/lista_ventas.html', {'comprobantes': comprobantes})
