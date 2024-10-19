5f2c82968861c0fde85bcc47e954be012f3955d3
Alex <chaffs25@gmail.com>
2024-10-19 19:34:06 -0300

    Revert "Feature/proveedores"

--- proveedores/models.py
+++ proveedores/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from productos.models import Insumo
-
 # Create your models here.
 class Proveedor (models.Model):
     razon_social = models.CharField(max_length=30)
@@ -18,29 +16,22 @@ class Pedido (models.Model):
         ('Recibido', 'Recibido'),
         ('Cancelado', 'Cancelado'),
     ]
-    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    
+    cantidad = models.PositiveSmallIntegerField()
     fecha = models.DateField()
     observacion = models.TextField()
     estado = models.CharField(
         choices= ESTADO,
         default='Pendiente',
     )
-
-class Item_Pedido(models.Model):
-    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='items_pedido')
-    insumo = models.ForeignKey(Insumo, on_delete=models.CASCADE)
-    cantidad = models.PositiveSmallIntegerField()
-
+    ##insumo
     
 class Recepcion (models.Model):
-    pedido = models.OneToOneField(Pedido, on_delete=models.CASCADE)
     fecha_recepcion = models.DateField()
+    ##insumo
+    cantidad_recibida = models.PositiveSmallIntegerField()
+    precio_unitario = models.DecimalField(max_digits=999, decimal_places=2)
+    precio_total = models.DecimalField(max_digits=9999, decimal_places=2)
     conformidad = models.TextField()
     observacion = models.TextField()
-    
-class Item_Recepcion(models.Model):
-    recepcion = models.ForeignKey(Recepcion, on_delete=models.CASCADE, related_name='items_recibido')
-    insumo = models.ForeignKey(Insumo, on_delete=models.CASCADE)
-    cantidad_recibida = models.PositiveSmallIntegerField()
-    precio_unitario = models.DecimalField(max_digits=8, decimal_places=2)
-    precio_total = models.DecimalField(max_digits=10, decimal_places=2)
+    pedido = models.OneToOneField(Pedido, on_delete=models.CASCADE)
